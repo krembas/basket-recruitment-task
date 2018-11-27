d72a5799866ee20212bb3b40b451e938eff8cce4
kr <kr@kr>
2018-11-27 17:26:45 +0100

    application & tests skeleton

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 
 def create_app():
     """Flask application factory"""
     app = Flask(__name__)
     app.secret_key = 'secretk@y'
+
+    @app.route('/basket', methods=['GET'])
+    def get_items():
+        """Get list of basket items"""
+        return jsonify()
+
+    @app.route('/basket', methods=['PUT'])
+    def update_items():
+        """Updates (add/remove) basket items"""
+        return jsonify()
+
+    @app.route('/basket', methods=['DELETE'])
+    def empty():
+        """Make basket empty"""
+        return jsonify()
 
     return app
 
--- tests.py
+++ tests.py
@@ -1,2 +1,26 @@
-def test():
-    assert 0 == 1
+import pytest
+
+from app import create_app
+
+
+@pytest.fixture
+def test_client():
+    app = create_app()
+    app.debug = True
+    return app.test_client()
+
+
+def test_basket(test_client):
+    with test_client.get('/basket') as resp:
+        assert resp.status_code == 200
+        assert resp.json == {}
+
+    with test_client.put('/basket') as resp:
+        assert resp.status_code == 200
+        assert resp.json == {}
+
+    with test_client.delete('/basket') as resp:
+        assert resp.status_code == 200
+        assert resp.json == {}
+
+
